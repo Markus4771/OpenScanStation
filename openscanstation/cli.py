a1683d6911c9b94cd90a78cc037276faccb815bb
Markus4771 <162835583+Markus4771@users.noreply.github.com>
2026-08-06 00:44:43 +0200

    CLI-Version auf 0.10.1 setzen

--- openscanstation/cli.py
+++ openscanstation/cli.py
@@ -9,7 +9,7 @@ import subprocess
 from pathlib import Path
 from openscanstation.scanner.manager import ScannerManager
 
-VERSION = "0.10.0"
+VERSION = "0.10.1"
 
 def _format_optional(value: bool | None) -> str:
     if value is None:
